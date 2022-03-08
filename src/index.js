2f67c25ce8bf40b5d5aa3245f1012c38064d560d
kunchao322 <chao.k@northeastern.edu>
2022-03-07 23:00:11 -0500

    add branch a6

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <App />,
+    document.getElementById('root')
+);	// core library
+// library for rendering into DOM element
+// application entry point, can be called anything, but App is common
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// use library to render
+// App
+// into element whose ID is root, the one declared earlier in index.html
+
