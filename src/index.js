aaa80391e9cddab53bbed7aca4274b93a1b8a64d
kunchao322 <chao.k@northeastern.edu>
2022-03-09 21:24:46 -0500

    `Completed React HelloWorld`

--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
 import React from 'react';
-import './index.css';
-import App from './App';
 import ReactDOM from 'react-dom';
+import App from './App';
 
 ReactDOM.render(
     <App />,
     document.getElementById('root')
-);	// core library
-// library for rendering into DOM element
-// application entry point, can be called anything, but App is common
-
-// use library to render
-// App
-// into element whose ID is root, the one declared earlier in index.html
-
+);
