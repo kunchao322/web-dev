bf4ecd298d9f11d52a0e490d6f77ca2df1dd520e
kunchao322 <chao.k@northeastern.edu>
2022-03-28 02:13:10 -0400

    used tuits json provided

--- src/component/Tuiter/tuit-list/tuit-list-item.js
+++ src/component/Tuiter/tuit-list/tuit-list-item.js
@@ -1,15 +1,26 @@
 const TuitListItem = ({
-                      post = {
-                          userName: "Loraine Suche",
-                          time: "17",
-                          content: "quis odio consequat varius integer ac leo pellentesque ultrices mattis odio donec vitae",
-                          title: "Woodland Rush",
-                          details: "lacinia aenean sit amet justo morbi ut odio cras mi pede malesuada in imperdiet et commodo vulputate justo in blandit ultrices enim lorem ipsum dolor sit",
-                          commentQty: "57",
-                          shareQty: "82",
-                          likeQty: "18",
-                          avatarIcon: "./images/pug.jpg",
-                          image: "../images/banner.jpeg"
+                      post =  {
+                          "_id": "123",
+                          "topic": "Web Development",
+                          "postedBy": {
+                              "username": "ReactJS"
+                          },
+                          "liked": true,
+                          "verified": false,
+                          "handle": "ReactJS",
+                          "time": "2h",
+                          "title": "React.js is a component based front end library that makes it very easy to build Single Page Applications or SPAs",
+                          "tuit": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+                          "attachments": {
+                              "video": "unKvMC3Y1kI"
+                          },
+                          "logo-image": "../../../images/react-blue.png",
+                          "avatar-image": "../../../images/react-blue.png",
+                          "stats": {
+                              "comments": 123,
+                              "retuits": 234,
+                              "likes": 345
+                          }
                       }
                   }) =>{
     const style1 = {
@@ -34,18 +45,18 @@ return (
     <>
         <div className="list-group-item d-flex flex-wrap align-top p-2" style={style1}>
             <div className="col-1 justify-content-end pe-1">
-                <a><img src={post.avatarIcon} className="img-fluid w-100 rounded-circle"/></a>
+                <a><img src={post["avatar-image"]} className="img-fluid w-100 rounded-circle"/></a>
             </div>
             <div className="col-11">
                 <ul className="list-group">
 
                     <li className="list-group-item bg-transparent border-0">
                         <div>
-                            <div className=" fw-bold ">{post.userName}<i className="fa fa-check-circle ps-1"></i><span
+                            <div className=" fw-bold ">{post.postedBy.username}<i className="fa fa-check-circle ps-1"></i><span
                                 className="text-secondary ps-1">- {post.time}</span>
                             </div>
                             <div className="text-white">
-                                {post.content}
+                                {post.tuit}
                             </div>
                         </div>
                     </li>
@@ -53,10 +64,10 @@ return (
                     <li className="list-group-item bg-transparent border-0">
                         <div className="card border-1 bg-transparent" style={style2}>
                             <img className="card-img-top" style={style3}
-                                 src={post.image} alt="Card image cap"/>
+                                 src={post.attachments} alt="Card image cap"/> TODO
                                 <div className="card-body ms-3 mb-3 me-3">
                                     <h5 className="card-title ">{post.title}</h5>
-                                    <p className="card-text">{post.details}</p>
+                                    <p className="card-text">{post.tuit}</p>
                                 </div>
                         </div>
                     </li>
@@ -64,17 +75,17 @@ return (
                     <li className="list-group-item d-inline-flex justify-content-evenly align-items-center bg-transparent border-0">
                         <div className=" d-flex wd-icons align-items-center" href="#">
                             <i className="fa fa-comment"></i>
-                            <div className="ps-1">{post.commentQty}</div>
+                            <div className="ps-1">{post.stats.comments}</div>
                         </div>
 
                         <div className="d-flex wd-icons align-items-center" href="#">
                             <i className="fa fa-reply-all"></i>
-                            <div className="ps-1">{post.shareQty}</div>
+                            <div className="ps-1">{post.stats.retuits}</div>
                         </div>
 
                         <div className="d-flex wd-icons  align-items-center" href="#" style={highlightedIcon}>
                             <i className="fa fa-heart"></i>
-                            <div className="ps-1">{post.likeQty}</div>
+                            <div className="ps-1">{post.stats.likes}</div>
                         </div>
 
                         <div className="d-flex wd-icons align-items-center" href="#">
